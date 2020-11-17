23a5d15a14f2bb093ce7668c49107c203f99dbd2
adrien_midol-monnet <adrien.midolmonnet@gmail.com>
2020-11-17 15:34:07 +0100

    Ajout du fichier 'application.yml' et modification permettant de faire des tests locaux de l'application

--- src/main/java/fr/tse/fise3/architecture_ntiers/Projet_stage/ProjetStageApplication.java
+++ src/main/java/fr/tse/fise3/architecture_ntiers/Projet_stage/ProjetStageApplication.java
@@ -1,9 +1,11 @@
 package fr.tse.fise3.architecture_ntiers.Projet_stage;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class ProjetStageApplication {
 
 	public static void main(String[] args) {
