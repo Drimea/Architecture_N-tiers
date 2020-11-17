3f287df2e5e1008fd25ac3d5a8f28427eadeef22
adrien_midol-monnet <adrien.midolmonnet@gmail.com>
2020-11-17 14:03:47 +0100

    Initialisation du repo

---
+++ src/main/java/fr/tse/fise3/architecture_ntiers/Projet_stage/ProjetStageApplication.java
@@ -0,0 +1,13 @@
+package fr.tse.fise3.architecture_ntiers.Projet_stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tse/fise3/architecture_ntiers/Projet_stage/ProjetStageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tse.fise3.architecture_ntiers.Projet_stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
